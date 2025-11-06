bcf18fcf7eb842c0aeb02157da583abca416c6d0
sanguyen12 <SanNVN.B23KH097@stu.ptit.edu.vn>
2025-11-06 10:35:50 +0700

    aaa

--- src/main/java/org/example/mst_medical_app/core/database/DatabaseConnection.java
+++ src/main/java/org/example/mst_medical_app/core/database/DatabaseConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DatabaseConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/medical_app";
     private static final String USER = "root"; // đổi nếu bạn dùng user khác
-    private static final String PASSWORD = "23112005"; // điền mật khẩu MySQL của bạn
+    private static final String PASSWORD = "Snguyen12"; // điền mật khẩu MySQL của bạn
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
